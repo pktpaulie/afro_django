e64ab4361c72450062ef5940fbc3477e955e6b5c
pauline <pktpaulie@gmail.com>
2023-04-12 09:04:42 +0300

    ADDING DJANGO TAGS Adding Django if tags

--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     p_title = 'Afro Django'
-    return render(request, 'index.html', {'p_title': p_title})
+    username = 'Pauline'
+    gender = 'Female'
+    return render(
+        request, 
+        'index.html', 
+        {'p_title': p_title,
+         'username': username,
+         'gender': gender})
