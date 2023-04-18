eef1e51d9147f49d58860a0f2d9dc7b7cf6bacaa
pauline <pktpaulie@gmail.com>
2023-04-18 12:08:08 +0300

    CREATE BASE TEMPLATE AND CUSTOM FLASH MESSAGES Adding base template and extending to other templates Adding flash messages

--- afro_dj/afro_dtl/urls.py
+++ afro_dj/afro_dtl/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
-    path('registration/', views.registration, name='registration')
+    path('registration/', views.registration, name='registration'),
+    path('login_user/', views.login_user, name = 'login_user'),
+    path('login_page/', views.login_page, name = 'login_page'),
+    path('logout_user/', views.logout_user, name = 'logout_user'),
 ]
--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -1,28 +1,98 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from .models import UserAccount
+from django.contrib.auth.models import User
 
 def index(request):
     p_title = 'Afro Django'
-    username = 'Pauline'
-    gender = 'Female'
-    return render(
-        request, 
-        'index.html', 
-        {'p_title': p_title,
-         'username': username,
-         'gender': gender})
+    
+    if request.user.is_authenticated:
+        username = request.user.username
+        context = {
+            'p_title': p_title,
+            'username': username,
+        }
+        return render(
+            request, 
+            'index.html', 
+            context=context)
+    else:    
+        author = 'Pauline'
+        gender = 'Female'
+        context = {
+            'p_title': p_title,
+            'author': author,
+            'gender': gender,
+        }
+        return render(request, 'index.html', context=context)
 
 def register(request):
     return render(request, 'register.html')
 
 def registration(request):
-    username = request.POST['username']
-    email = request.POST['email']
+    user_name = request.POST['username']
+    email = request.POST['user_email']
     password = request.POST['password']
     gender = request.POST['gender']
-    user_details = [username, email, password, gender]
+    user_details = [user_name, email, password, gender]
     print(user_details)
-    return render(request, 'index.html', {'username': username})
+    if User.objects.filter(username=user_name).first():
+        print(user_name + ' already exists')
+        return render(request, 'login.html')
+    else:
+        user = User.objects.create_user(user_name, email, password)
+        return render(request, 'login.html')
+
+    #return render(request, 'index.html', {'username': username})
     
-    
+def login_user(request):
+    user_name = request.POST['username']
+    pwd = request.POST['password']
+    if User.objects.filter(username=user_name):
+        print(user_name + ' already exists')
+        logged_user = authenticate(request, username=user_name, password=pwd)
+        if logged_user is not None:
+            #login the user
+            auth_login(request, logged_user)
+            print(user_name + ' ' + 'logged in successfully')
+            return redirect('index')
+        else:
+            # where authentication has failed/ user credentials do not exist
+            return render(request, 'login.html')
+    else:
+        print('User credentials do not exist')
+        return render(request, 'login.html')
+
+
+
+def login_page(request):
+    return render(request, 'login.html')
+
+
+'''
+if request=='POST':
+        user_name = request.POST['username']
+        pwd = request.POST['password']
+        if User.objects.filter(username=user_name).first():
+            print(user_name + ' already exists')
+            logged_user = authenticate(request, username=user_name, password=pwd)
+            if logged_user is not None:
+                #login the user
+                auth_login(request, logged_user)
+                print(user_name + ' ' + 'logged in successfully')
+                return redirect('index/')
+            else:
+                # where authentication has failed/ user credentials do not exist
+                return render(request, 'login.html')
+        else:
+            print('User credentials do not exist')
+            return render(request, 'login.html')
+    else:
+        pass
+'''
+
+@login_required
+def logout_user(request):
+    auth_logout(request)
+    return redirect('login_page')
